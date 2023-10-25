215de1787766b024dd9eb9b6542027750fbe3262
Sujal Patel <sujalpatel1882@gmail.com>
2023-10-25 20:07:24 +0530

    Initial commit

---
+++ app/src/main/java/com/sujalpatel/mad_pr_10_21012021087/PersonAdapter.kt
@@ -0,0 +1,33 @@
+package com.sujalpatel.mad_pr_10_21012021087
+
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.material.button.MaterialButton
+import java.util.ArrayList
+
+class PersonAdapter(context:Context,val array:ArrayList<Person>):ArrayAdapter<Person>(context,0,array) {
+
+
+
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.person_item,parent,false)
+        val person_data = array.get(position)
+        view.findViewById<TextView>(R.id.textView_name).text=person_data.name
+        view.findViewById<TextView>(R.id.textView_no).text=person_data.phoneNo
+        view.findViewById<TextView>(R.id.textView_email).text=person_data.emailId
+        view.findViewById<TextView>(R.id.textView_address).text=person_data.address
+        view.findViewById<MaterialButton>(R.id.btn_location).setOnClickListener {
+            Intent(parent.context,MapsActivity::class.java).putExtra("Object",person_data).apply { context.startActivity(this) }
+        }
+        return view
+    }
+
+}
